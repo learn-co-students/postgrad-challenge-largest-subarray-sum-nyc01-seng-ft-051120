9d3a9189bafc46c248e32e1e7c99d2275456a938
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-09-01 11:28:21 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
+ 
+function largestSubarraySum(array){
+  let currentSum = 0
+  let largestSum = 0
+
+  for (let number of array) {
+      currentSum = Math.max(0, (currentSum + number))
+      largestSum = Math.max(largestSum, currentSum)
+  }
+  return largestSum
+}
+ 
+largestSubarraySum(array)
